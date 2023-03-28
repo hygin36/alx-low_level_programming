5ee54aaf99d24a61b45adf4712aa89d80040d9ec
Hygin <rahygin@gmail.com>
2023-03-27 19:32:12 -0700

    Rectifying betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Takes a pointer to a int and updatest eh value it points to to 98
+ * reset_to_98 - Takes a pointer to a int
+ * and updatest eh value it points to to 98
  *
  * @n: an integer input pointer
  *
